f81edbc8db212def04cedc1c34552465f3606360
Troy Comi <troycomi@gmail.com>
2026-08-14 07:54:29 -0400

    fix: remove dead to_json method

--- src/slurmise/job_data.py
+++ src/slurmise/job_data.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import json
 from dataclasses import astuple, dataclass, field
 
 import h5py
@@ -90,13 +89,6 @@ class JobData:
             memory=memory,
             runtime=runtime,
         )
-
-    def to_json(self):
-        result = {
-            "categories": self.categories,
-            "numerics": self.numerics,
-        }
-        return json.dumps(result)
 
     def __eq__(self, other):
         return dc_eq(self, other)
